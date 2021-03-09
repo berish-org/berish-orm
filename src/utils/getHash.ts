cadcc0b27b8a2a9749b866599510665571ac6153
Berishev Ravil <quadraticteam@gmail.com>
2021-03-10 02:07:44 +0300

    Update hash typing

--- src/utils/getHash.ts
+++ src/utils/getHash.ts
@@ -2,6 +2,6 @@ import * as crypto from 'crypto-js';
 
 export function getHash(obj: { [key: string]: any }) {
   const strJSON = JSON.stringify(obj || {});
-  const words = crypto.SHA256(strJSON, strJSON);
+  const words = crypto.SHA256(strJSON);
   return words.toString();
 }
